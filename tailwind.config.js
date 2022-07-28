58586e50f21add9e1c0eb4dbf50b80f75f47b4d4
Bhakti646 <bhaktinikam01@gmail.com>
2022-07-28 10:26:47 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    extend: {
+      colors: {
+        bgColor:'rgb(245, 242, 255)',
+        brightPurple: '#5123CB',
+        darkGrayishBlue: '#2F3F52',
+        myDarkBlue:'#000066',
+        postBg:'#0B2855',
+        postIconBg:'#525086',
+      },
+      fontFamily: {
+        poppins:['Poppins']
+      },
+    },
+  },
+  plugins: [],
+}
