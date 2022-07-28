147d9a31d65692f6770f26a262db3a2f937de584
saintBeingDev <bhaktinikam01@gmail.com>
2022-07-28 12:13:10 +0530

    added article reaction

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,6 +16,8 @@ module.exports = {
         myDarkBlue:'#000066',
         postBg:'#0B2855',
         postIconBg:'#525086',
+        reactionIconBg:'#F1F5F9',
+        reactionText:'#334155'
       },
       fontFamily: {
         poppins:['Poppins']
